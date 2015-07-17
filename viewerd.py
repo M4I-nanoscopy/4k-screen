75c07f2ba17ce5ee676b961c2ebf6b2bb0757ce4
Hugo Boulanger <hugo.boulanger.oib@gmail.com>
2015-07-17 10:30:34 +0200

    variables for paths

--- viewerd.py
+++ viewerd.py
@@ -16,7 +16,11 @@ files = []
 user_files = []
 max_sleep_time = 120
 min_sleep_time = 10
-mid_sleep_time = 30
+def_sleep_time = 30
+drop_img_path = "/mnt/serverscratch/4k-screen/drop_images_here"
+raw_img_path = "/mnt/serverscratch/4k-screen/rawdata"
+sorted_raw_img_path = raw_img_path + "/"
+
 
 def sleep(t):
     print "--- sleeping... ---"
@@ -27,30 +31,30 @@ def sleep(t):
 while 1:
 
     # Scan for images in drop dir, else show raw data
-    user_files = os.listdir("/mnt/serverscratch/4k-screen/drop_images_here")
+    user_files = os.listdir(drop_img_path)
     if len(user_files) > 0:
         print "Full screen mode for user images"
-        feh.full_screen("/mnt/serverscratch/4k-screen/drop_images_here")
+        feh.full_screen(drop_img_path)
 
-        sleep(mid_sleep_time)
+        sleep(def_sleep_time)
         continue
 
     if datetime.datetime.now().hour > 20 or datetime.datetime.now().hour < 7:
 	print "Outside office hours, go home!"
-	sleep(mid_sleep_time)
+	sleep(def_sleep_time)
 	continue
 
     print "Raw data mode in 100% zoom"
 
     # Only refresh the directory listing every 6 iterations, or when the file list is empty
-    files = os.listdir("/mnt/serverscratch/4k-screen/rawdata")
+    files = os.listdir(raw_img_path)
 
-    files.sort(key=lambda x: os.path.getctime("/mnt/serverscratch/4k-screen/rawdata/" + x))
+    files.sort(key=lambda x: os.path.getctime(sorted_raw_img_path + x))
     
     num_of_files = len(files)
 
     if num_of_files == 0:
-        sleep(mid_sleep_time)
+        sleep(def_sleep_time)
         continue
 
     current = files.pop(0)
@@ -61,9 +65,9 @@ while 1:
 	sleep(min_sleep_time)
         continue
 
-    if os.path.exists("/mnt/serverscratch/4k-screen/rawdata/" + current):
+    if os.path.exists(sorted_raw_img_path + current):
         print "Show %s" % current
-        feh.zoom_100(current, "/mnt/serverscratch/4k-screen/rawdata/")
+        feh.zoom_100(current, sorted_raw_img_path)
     else:
         print "File %s does not exist. Weird! System overloaded?" % current
         sleep(min_sleep_time)
@@ -77,7 +81,7 @@ while 1:
         sleep(sleep_time)
 
     try:
-        os.remove("/mnt/serverscratch/4k-screen/rawdata/" + current)
+        os.remove(sorted_raw_img_path + current)
     except OSError, e:
         print "File already deleted."
         pass
