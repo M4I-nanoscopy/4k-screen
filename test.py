8b53d10edb40a39a496ec75b0b147bd6e980691d
Boulanger Hugo (M4I) <h.boulanger@FHMLM4I-WKS003.unimaas.nl>
2015-07-31 12:12:30 +0200

    added no_doubles test

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ import shutil
 import subprocess
 import shlex
 from config import *
+from testconfig import *
 from scannerd import *
 
 
@@ -23,7 +24,7 @@ class ScanQueueTest(unittest.TestCase):
 
     def tearDown(self):
         self.queue = None
-        subprocess.call(shlex.split('/usr/bin/tsp -n rm -d %s/test' % SCREEN_DIR))
+        subprocess.call(shlex.split('rm -d %s/test' % SCREEN_DIR))
 
 class ScanText2DictTest(unittest.TestCase):
 
@@ -34,7 +35,6 @@ class ScanText2DictTest(unittest.TestCase):
         tfb = open('%s/test_file_bool.txt' % SCREEN_DIR, 'w')
         tfb.write('test###True\nest###False\n')
         tfb.close()
-        time.sleep(1)
 
     def test_list(self):
         self.expected_list_dict = {'test' : ['0','1','2','3'], 'est' : ['4']}
@@ -47,12 +47,34 @@ class ScanText2DictTest(unittest.TestCase):
         self.assertEqual( self.expected_bool_dict, self.bool_dict)
 
     def tearDown(self):
-        subprocess.call(shlex.split('/usr/bin/tsp -n rm %s/test_file_list.txt' % SCREEN_DIR))
-        subprocess.call(shlex.split('/usr/bin/tsp -n rm %s/test_file_bool.txt' % SCREEN_DIR))
+        subprocess.call(shlex.split('rm %s/test_file_list.txt' % SCREEN_DIR))
+        subprocess.call(shlex.split('rm %s/test_file_bool.txt' % SCREEN_DIR))
         self.expected_list_dict = None
         self.list_dict = None
         self.expected_bool_dict = None
         self.bool_dict = None
+
+class ScanNoDoublesTest(unittest.TestCase):
+
+    def setUp(self):
+        self.img_origin = img_dir + big_tiff
+        self.img_rawdata = RAWDATA + rawdata_ro_img_dir + big_tiff
+        self.img_screen = raw_img_path + big_tiff
+        subprocess.call(shlex.split("/bin/cp '%s' '%s'" % (self.img_origin, self.img_rawdata)))
+        subprocess.call(shlex.split("/bin/cp '%s' '%s'" % (self.img_origin, self.img_screen)))
+
+    def test_no_doubles(self):
+        self.no_doubles = no_doubles(self.img_rawdata)
+        self.assertNotEqual(self.no_doubles, self.img_screen)
+
+    def tearDown(self):
+        subprocess.call(shlex.split("rm %s" % (self.img_rawdata)))
+        subprocess.call(shlex.split("rm %s" % (self.img_screen)))
+        self.img_origin = None
+        self.img_rawdata = None
+        self.img_screen = None
+        self.no_doubles = None
+
 
 if __name__ == '__main__':
     unittest.main()
